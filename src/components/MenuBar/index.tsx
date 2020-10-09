9759a458f98bead879ac03ecafc398e4121e9866
pedromaranini <pedrolucasmaranini30@gmail.com>
2020-10-09 15:48:17 -0300

    menubar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,11 +1,69 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Button from '../Button';
+
+import { Container,
+        Topside,
+        Logo,
+        MenuButton,
+        HomeIcon,
+        BellIcon,
+        EmailIcon,
+        FavoriteIcon,
+        ProfileIcon,
+        Botside,
+        Avatar,
+        ProfileData,
+        ExitIcon,
+} from './styles';
 
 const MenuBar: React.FC = () => {
   return (
     <Container>
-      <h1>MenuBar</h1>
+      <Topside>
+        <Logo />
+
+        <MenuButton>
+          <HomeIcon />
+            <span>Página Inicial</span>
+        </MenuButton>
+
+        <MenuButton>
+          <BellIcon />
+            <span>Notificações</span>
+        </MenuButton>
+
+        <MenuButton>
+          <EmailIcon />
+            <span>Mensagens</span>
+        </MenuButton>
+
+        <MenuButton>
+          <FavoriteIcon />
+            <span>Favoritados</span>
+        </MenuButton>
+
+        <MenuButton className="active" >
+          <ProfileIcon />
+            <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+
+      </Topside>
+
+      <Botside>
+        <Avatar />
+
+        <ProfileData>
+          <strong>Pedro Maranini</strong>
+          <span>@m4ranini</span>
+        </ProfileData>
+
+        <ExitIcon />
+      </Botside>
     </Container>
   );
 };
